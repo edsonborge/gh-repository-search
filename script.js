ca7491c23ec79d6d34a13adc45edffd61f32a9ad
Edson Borge <edsonborge90@gmail.com>
2020-10-26 15:38:56 -0300

    Fixed search; 404 redirect added

--- script.js
+++ script.js
@@ -2,35 +2,35 @@ const srcInput = document.querySelector('.search-input');
 const srcButton = document.querySelector('.search-btn');
 const gridRepos = document.querySelector('.grid-repos');
 const searchForm = document.querySelector('.search-form');
+const loading = document.querySelector('.loading');
 const inputText = '';
  
 const createCard = (name, description) => {
   const repoCard = document.createElement('div');
   const repoLink = document.createElement('a');
   const repoTxt = document.createElement('p');
-
+  
   repoCard.className = 'repo-card';
-
+  
   repoLink.setAttribute(`href`, `https://github.com/${inputTxt}/${name}`);
   repoLink.textContent = name;
-
+  
   repoTxt.textContent = description || '';
-
+  
   repoCard.appendChild(repoLink);
   repoCard.appendChild(repoTxt);
-
+  
   gridRepos.appendChild(repoCard);
 }
 
 const searchRepos = (e) => {
   e.preventDefault();
   inputTxt = srcInput.value;
-
+  
   axios.get(`https://api.github.com/users/${inputTxt}/repos`)
   .then(response => showRepos(response.data))
-  .catch(error => console.warn(error));
-
-  return false;
+  .catch(error => window.location.href = 'https://github.com/404')
+  .then(() => loading.classList.add('loading'));
 }
 
 const showRepos = (repos) => {
@@ -39,4 +39,8 @@ const showRepos = (repos) => {
   }
 }
 
-searchForm.addEventListener('submit', searchRepos);
+searchForm.addEventListener('submit', event => {
+  loading.classList.remove('loading');
+  gridRepos.innerText = '';
+  searchRepos(event);
+});
